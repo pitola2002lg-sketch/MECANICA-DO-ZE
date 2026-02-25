13e059e1d967272fe021fbc9858ea17f6263021a
pitola2002lg-sketch <pitola2002lg@gmail.com>
2026-02-25 13:38:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,41 @@
-const CACHE = 'mecanica-ze-v1';
-const ASSETS = ['./index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+const CACHE_NAME = 'mecanica-ze-v1';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// install
+self.addEventListener('install', e=>{
+ e.waitUntil(
+  caches.open(CACHE_NAME).then(cache=>{
+   return cache.addAll(FILES_TO_CACHE);
+  })
+ );
+ self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+// activate
+self.addEventListener('activate', e=>{
+ e.waitUntil(
+  caches.keys().then(keys=>{
+   return Promise.all(
+    keys.filter(k=>k!==CACHE_NAME)
+    .map(k=>caches.delete(k))
+   );
+  })
+ );
+ self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+// fetch
+self.addEventListener('fetch', e=>{
+ e.respondWith(
+  caches.match(e.request).then(res=>{
+   return res || fetch(e.request);
+  })
+ );
 });
